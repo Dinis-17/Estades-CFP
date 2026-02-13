d562e5d1623e2fbe1a5eb01b0d92f3f1baf01d60
Dinis-17 <dinismedeiros1706@gmail.com>
2026-02-13 09:45:01 +0100

    Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <>
-      <div>Header</div>
-    </>
+    <header className="main-header">
+      <div className="header-content">
+        <div className="logo">
+          <NavLink to="/">Estades CFP</NavLink>
+        </div>
+      </div>
+    </header>
   );
 };
 
